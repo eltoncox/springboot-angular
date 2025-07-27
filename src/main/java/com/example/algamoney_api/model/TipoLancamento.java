b3f94931cbb46bbea9065276c598286ea558bbc2
eltoncox <elton.unix@gmail.com>
2025-07-27 17:04:44 -0300

    Lista e busca de lançamentos.

---
+++ src/main/java/com/example/algamoney_api/model/TipoLancamento.java
@@ -0,0 +1,6 @@
+package com.example.algamoney_api.model;
+
+public enum TipoLancamento {
+    RECEITA,
+    DESPESA
+}
---
+++ src/main/java/com/example/algamoney_api/repository/lancamento/LancamentoRepositoryQuery.java
@@ -0,0 +1,5 @@
+package com.example.algamoney_api.repository.lancamento;
+
+public interface LancamentoRepositoryQuery {
+
+}
