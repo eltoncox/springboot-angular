0691dbd2bafd0cf951f904b6ec896d41254fe8d6
eltoncox <elton.unix@gmail.com>
2025-07-27 00:51:51 -0300

    recurso GET com findById, e POst com status created.

--- src/main/java/com/example/algamoney_api/resource/CategoriaResource.java
+++ src/main/java/com/example/algamoney_api/resource/CategoriaResource.java
@@ -1,11 +1,20 @@
 package com.example.algamoney_api.resource;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.algamoney_api.model.Categoria;
 import com.example.algamoney_api.repository.CategoriaRepository;
@@ -21,6 +30,35 @@ public class CategoriaResource {
 	public List<Categoria> listar() {
 		return categoriaRepository.findAll();
 	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria,HttpServletResponse response) {
+		Categoria  categoriaSalva = categoriaRepository.save(categoria);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+			.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+	
+	/**
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
+	Optional<Categoria> categoria = this.categoriaRepository.findById(codigo);
+	return categoria.isPresent() ? 
+	        ResponseEntity.ok(categoria.get()) : ResponseEntity.notFound().build();
+	}*/
+	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
+	return this.categoriaRepository.findById(codigo)
+	  .map(categoria -> ResponseEntity.ok(categoria))
+	  .orElse(ResponseEntity.notFound().build());
+	}
+	
+	// 3.8. Validando atributos desconhecidos
+	
 	
 	
 
