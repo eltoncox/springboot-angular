45331010115202e045afbf2e95447a49db86869c
eltoncox <elton.unix@gmail.com>
2025-07-26 23:05:57 -0300

    recurso GET, Entidade Categoria, CategoriaRepository.

---
+++ src/main/java/com/example/algamoney_api/resource/CategoriaResource.java
@@ -0,0 +1,27 @@
+package com.example.algamoney_api.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.algamoney_api.model.Categoria;
+import com.example.algamoney_api.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+	
+	@Autowired
+	public CategoriaRepository categoriaRepository;	
+	
+	@GetMapping
+	public List<Categoria> listar() {
+		return categoriaRepository.findAll();
+	}
+	
+	
+
+}
