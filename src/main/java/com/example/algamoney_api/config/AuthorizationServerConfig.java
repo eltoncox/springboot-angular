2264f00e777e6471cbfa552eac5a82fc8abbb5f2
eltoncox <elton.unix@gmail.com>
2025-08-06 18:52:08 -0300

    Movendo o usuário para o banco de dados.

--- src/main/java/com/example/algamoney_api/config/AuthorizationServerConfig.java
+++ src/main/java/com/example/algamoney_api/config/AuthorizationServerConfig.java
@@ -31,12 +31,12 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory()
-				.withClient("angular")
-				.secret(passwordEncoder.encode("@ngul@r0")) // @ngul@r0
-				.scopes("read", "write")
-				.authorizedGrantTypes("password", "refresh_token")
-				.accessTokenValiditySeconds(15)
-				.refreshTokenValiditySeconds(3600 * 24);
+		        .withClient("angular")
+		        .secret("$2a$10$G1j5Rf8aEEiGc/AET9BA..xRR.qCpOUzBZoJd8ygbGy6tb3jsMT9G")
+		        .scopes("read", "write")
+		        .authorizedGrantTypes("password", "refresh_token")
+		        .accessTokenValiditySeconds(1800)
+		        .refreshTokenValiditySeconds(3600 * 24);
 	}
 
 	@Override
