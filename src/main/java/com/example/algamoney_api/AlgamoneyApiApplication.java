f6c02bc009fb10c16776737c14c0709b9d1f44e3
eltoncox <elton.unix@gmail.com>
2025-08-07 16:24:21 -0300

    Profiles do Spring.

--- src/main/java/com/example/algamoney_api/AlgamoneyApiApplication.java
+++ src/main/java/com/example/algamoney_api/AlgamoneyApiApplication.java
@@ -2,13 +2,17 @@ package com.example.algamoney_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import com.example.algamoney_api.config.property.AlgamoneyApiProperty;
+
 @SpringBootApplication // 
+@EnableConfigurationProperties(value = AlgamoneyApiProperty.class)
 public class AlgamoneyApiApplication {
 
 	public static void main(String[] args) {
